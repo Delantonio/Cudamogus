578da1a83cb39ecb2b7abcb318d79a1f2f7e209f
Antoine Delattre <antoine.delattre@epita.fr>
2023-01-02 21:54:58 +0100

    Small refacto: separating versions

---
+++ src/baseline.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "image.hh"
+#include "pipeline.hh"
+#include "fix_cpu.hh"
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <filesystem>
+#include <numeric>
+
+int cpu_main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]);
---
+++ src/cub_version.cuh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "image.hh"
+#include "pipeline.hh"
+#include "cuda_utils.cuh"
+
+#include <cub/cub.cuh>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <filesystem>
+#include <numeric>
+
+int cub_main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]);
